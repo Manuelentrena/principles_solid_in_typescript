e28b55e732ee022288aa9ba16bb33c891134c19e
EntrenaDev <manuel.entrena@gmail.com>
2023-10-03 21:11:52 +0200

    first refactor, using swicth

--- kata_gildedRose/app/gilded-rose.ts
+++ kata_gildedRose/app/gilded-rose.ts
@@ -1,3 +1,8 @@
+const ENTRADA = "Backstage passes to a TAFKAL80ETC concert";
+const QUESO = "Aged Brie";
+const SULFURO = "Sulfuras, Hand of Ragnaros";
+const CONJURO = "Conjured";
+
 export class Item {
   name: string;
   sellIn: number;
@@ -18,52 +23,53 @@ export class GildedRose {
   }
 
   updateQuality(): Item[] {
+    let newQuality = 0;
     for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name !== 'Aged Brie' && this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
-        if (this.items[i].quality > 0) {
-          if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-          if (this.items[i].name === 'Conjured' && this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1
-          }
-        }
-      } else {
-        if (this.items[i].quality < 50) {
-          this.items[i].quality = this.items[i].quality + 1
-          if (this.items[i].name === 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].sellIn < 11) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-            if (this.items[i].sellIn < 6) {
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1
-              }
-            }
-          }
-        }
-      }
-      if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
-      }
-      if (this.items[i].sellIn < 0) {
-        if (this.items[i].name !== 'Aged Brie') {
-          if (this.items[i].name !== 'Backstage passes to a TAFKAL80ETC concert') {
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name !== 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].quality = this.items[i].quality - 1
-              }
-            }
-          } else {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
-          }
-        } else {
+      switch (this.items[i].name) {
+        case QUESO:
           if (this.items[i].quality < 50) {
             this.items[i].quality = this.items[i].quality + 1
           }
-        }
+
+          if (this.items[i].quality < 50 && this.items[i].sellIn <= 0) {
+            this.items[i].quality = this.items[i].quality + 1
+          }
+
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          break;
+        case ENTRADA:
+          if (this.items[i].sellIn > 10) {
+              newQuality = 1;
+          } else if (this.items[i].sellIn >= 6) {
+              newQuality = 2;
+          } else if (this.items[i].sellIn > 0) {
+              newQuality = 3;
+          } else {
+              this.items[i].quality = 0;
+          }
+
+          this.items[i].quality = Math.min(50, this.items[i].quality + newQuality);
+          this.items[i].sellIn--;
+          break;
+        case SULFURO:
+
+          break;
+        case CONJURO:
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          break;
+        default:
+          if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 1
+          }
+
+          this.items[i].sellIn = this.items[i].sellIn - 1;
+          break;
       }
     }
 
